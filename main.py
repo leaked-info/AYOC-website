54718020232892d161a9ffb3b9e311922d7d5a8d
httpssocketv2 <105125827+httpssocketv2@users.noreply.github.com>
2022-05-08 23:46:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def stylecss():
 
 @app.route('/')
 def hello_world():
-response = Response()
-response.headers{'link'] = '<style.css>; rel=stylesheet;'
-response.headers['Refresh'] = '5; url=http://sandiegofreepress.org/wp-content/uploads/2015/05/giphy.gif
-return response
+    response = Response()
+    response.headers['link'] = '<style.css>; rel=stylesheet;'
+    response.headers['Refresh'] = '5; url=http://sandiegofreepress.org/wp-content/uploads/2015/05/giphy.gif'
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
